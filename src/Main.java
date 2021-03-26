b66f85052c4e8566c778c2a147b2dc278ca2acd4
MaIsabelSolano <misolanob@gmail.com>
2021-03-26 01:55:06 -0600

    .

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,27 @@
-public class Main {
-    Factory<String,String> factory = new Factory<>();
+import java.util.*;
 
+class Main{
+    public static void main(String[] args){
+        Factory factory = new Factory();
+        Scanner scan = new Scanner(System.in);
+
+        //inicio del programa
+        int opcion = 0;
+        pr("Bienvenido al programa");
+        while (opcion != 5){
+            try {
+
+
+            }
+            catch (InputMismatchException ime){
+                pr("Ha ocurrido un error, el valor ingresado no es valido");
+                scan.nextLine();
+
+            }
+        }
+    }
+
+    public static void pr (String a){
+        System.out.println(a);
+    }
 }
